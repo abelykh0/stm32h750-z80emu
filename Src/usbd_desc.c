b27755877c5df4fe3704cd169532c86c9cd4a6a2
Andrey Belykh <abelykh@yahoo.ca>
2025-11-09 14:04:27 -0500

    Now correctly detected as camera

--- Src/usbd_desc.c
+++ Src/usbd_desc.c
@@ -62,13 +62,13 @@
   * @{
   */
 
-#define USBD_VID     1155
+#define USBD_VID     0xC251
 #define USBD_LANGID_STRING     1033
 #define USBD_MANUFACTURER_STRING     "STMicroelectronics"
-#define USBD_PID_FS     22336
+#define USBD_PID_FS     0x1706
 #define USBD_PRODUCT_STRING_FS     "STM32 Camera"
-#define USBD_CONFIGURATION_STRING_FS     "CDC Config"
-#define USBD_INTERFACE_STRING_FS     "CDC Interface"
+#define USBD_CONFIGURATION_STRING_FS     "Camera Config"
+#define USBD_INTERFACE_STRING_FS     "Camera Interface"
 
 #define USB_SIZ_BOS_DESC            0x0C
 
