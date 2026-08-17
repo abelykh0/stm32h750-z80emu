abcf667ea94cd7120341e7876789425ad8652bec
Andrey Belykh <Andrey.Belykh@acumatica.com>
2026-08-17 16:37:53 -0400

    Fix

--- Inc/main.h
+++ Inc/main.h
@@ -31,7 +31,6 @@ extern "C" {
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "screen/config.h"
 /* USER CODE END Includes */
 
 /* Exported types ------------------------------------------------------------*/
